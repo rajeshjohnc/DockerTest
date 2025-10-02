d27fae7525acea186c7b3d10e9febb34758fe588
rajeshjohnc <rajeshjohnc@gmail.com>
2025-10-02 22:49:21 +0530

    8080

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
-var app = builder.Build();
 builder.WebHost.UseUrls("http://localhost:8081");
+var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
